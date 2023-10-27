82ae3077a2f88dd501879f5a4eb087845d87e2af
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-10-28 01:14:45 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,23 +2,5 @@ LATEST_VERSION=$(curl -s https://api.github.com/repos/Cog-Creators/Lavalink-Jars
 echo Downloading Lavalink.jar version $LATEST_VERSION...
 curl -LJO "https://github.com/Cog-Creators/Lavalink-Jars/releases/download/$LATEST_VERSION/Lavalink.jar"
 echo Lavalink.jar is downloaded...
-clear
 echo Starting Lavalink...
 java -jar Lavalink.jar &
-clear
-
-# Wait for Lavalink to be ready
-while true; do
-    response=$(curl -s -o /dev/null -w "%{http_code}" https://lavalink.fragmetal88.repl.co/metrics)
-    if [ "$response" = "200" ]; then
-        break
-    fi
-    sleep 5
-done
-
-echo "Lavalink is ready. Starting loop..."
-
-while true; do
-    response=$(curl -s -o /dev/null -w "%{http_code}" https://lavalink.fragmetal88.repl.co/metrics)
-    sleep 5
-done
