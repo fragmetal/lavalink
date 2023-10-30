bd656f8b671953749e1f76ceede86e8cfe20d5c6
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-10-30 22:54:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
-if [ ! -f Lavalink.jar ]; then
-    echo "Lavalink.jar does not exist, downloading..."
-    download_url=$(curl -s https://api.github.com/repos/lavalink-devs/Lavalink/releases/latest | grep 'browser_' | cut -d\" -f4)
-    curl -L $download_url -o Lavalink.jar
-else
-    echo "Lavalink.jar already exists."
-fi
-clear
 echo "Running Lavalink.jar..."
 java -jar Lavalink.jar
