bba44045db841da95ccc22cfc882e6a0d4af78dd
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-10-31 13:10:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,19 @@
+#!/bin/bash
+
+# Add the repository
+echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu focal main" | tee /etc/apt/sources.list.d/linuxuprising-java.list
+
+# Import the GPG key
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
+
+# Update package lists
+apt-get update
+
+# Install Java 16
+apt-get install -y oracle-java16-installer
+
+# Verify the installation
+java -version
+
 echo "Running Lavalink.jar..."
 java -jar Lavalink.jar
