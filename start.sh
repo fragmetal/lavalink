e1ede5bf64499b7e01f994e03706c5520198a60a
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-08 22:00:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-
 # Check if Java OpenJDK 17 is installed
 if [[ ! -d jdk-17 ]] || [[ -z $(ls -A jdk-17) ]]; then
-    java_version=$(java -version 2>&1 | grep 'openjdk version' | awk '{print $3}' | tr -d '\"')
-    if [[ $java_version != "17"* ]]; then
-        echo "Java OpenJDK 17 is not installed, downloading..."
-        # Download OpenJDK 17
-        wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
-        tar -xvf openjdk-17_linux-x64_bin.tar.gz
-        rm openjdk-17_linux-x64_bin.tar.gz
-        # Move the OpenJDK files to the 'jdk-17' directory
-        mv jdk-17* jdk-17
-        # Clear the console
-        clear
-    else
-        echo "Java OpenJDK 17 is already installed."
-    fi
+    echo "Java OpenJDK 17 is not installed, downloading..."
+    # Download OpenJDK 17
+    wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
+    tar -xvf openjdk-17_linux-x64_bin.tar.gz
+    rm openjdk-17_linux-x64_bin.tar.gz
+    # Move the OpenJDK files to the 'jdk-17' directory
+    mv jdk-17* jdk-17
+    # Clear the console
+    clear
 fi
 # GitHub repo URL
 repo_url="https://api.github.com/repos/lavalink-devs/Lavalink/releases/latest"
 
 # Get the URL of the latest release from the GitHub API
 get_latest_release_url() {
-    curl --silent "$repo_url" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/'
+    # Fetch the API response
+    api_response=$(curl --silent "$repo_url")
+    
+    # Print the API response for debugging
+    echo "API response: $api_response"
+    
+    # Extract and print the download URL
+    download_url=$(echo "$api_response" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo "Extracted download URL: $download_url"
+    
+    # Return the download URL
+    echo "$download_url"
 }
 
 # Download and run Lavalink.jar
