a4d8fbd8b03b9169fc2d3df0958eb1488a646a24
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-08 21:28:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Check if Java OpenJDK 17 is installed
 if [[ ! -d jdk-17 ]] || [[ -z $(ls -A jdk-17) ]]; then
     java_version=$(java -version 2>&1 | grep 'openjdk version' | awk '{print $3}' | tr -d '\"')
@@ -8,23 +9,31 @@ if [[ ! -d jdk-17 ]] || [[ -z $(ls -A jdk-17) ]]; then
         wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
         tar -xvf openjdk-17_linux-x64_bin.tar.gz
         rm openjdk-17_linux-x64_bin.tar.gz
+        # Move the OpenJDK files to the 'jdk-17' directory
+        mv jdk-17* jdk-17
         # Clear the console
         clear
     else
         echo "Java OpenJDK 17 is already installed."
     fi
 fi
+
 # Print the version of Java being used
 ./jdk-17/bin/java -version
+
 # Download and run Lavalink.jar
 if [ ! -f Lavalink.jar ]; then
     echo "Lavalink.jar does not exist, downloading..."
-    download_url=$(curl -s https://api.github.com/repos/lavalink-devs/Lavalink/releases/latest | grep 'browser_' | cut -d\" -f4)
-    curl -L $download_url -o Lavalink.jar
+    download_url=$(curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/latest | jq -r '.assets[].browser_download_url')
+    curl -L -o Lavalink.jar $download_url
 else
     echo "Lavalink.jar already exists."
 fi
-echo "Running Lavalink.jar"
-./jdk-17/bin/java -jar Lavalink.jar
-# Run the Flask server
-# ./Python-3.10.10/_install/bin/python3 flaskme.py
+
+# Check if Lavalink.jar was successfully downloaded
+if [ -f Lavalink.jar ]; then
+    echo "Running Lavalink.jar"
+    ./jdk-17/bin/java -jar Lavalink.jar
+else
+    echo "Lavalink.jar download failed."
+fi
